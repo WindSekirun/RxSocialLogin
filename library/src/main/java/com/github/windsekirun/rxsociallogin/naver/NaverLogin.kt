77dda8fc67e54036b98e2dce15902a769b151c64
Pyxis <pyxis@uzuki.live>
2018-09-16 15:17:55 +0900

    Migrate OKHttp to Fuel - naver (issue #9)

--- library/src/main/java/com/github/windsekirun/rxsociallogin/naver/NaverLogin.kt
+++ library/src/main/java/com/github/windsekirun/rxsociallogin/naver/NaverLogin.kt
@@ -3,15 +3,15 @@ package com.github.windsekirun.rxsociallogin.naver
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import com.github.kittinunf.fuel.httpGet
 import com.github.windsekirun.rxsociallogin.RxSocialLogin
 import com.github.windsekirun.rxsociallogin.SocialLogin
-import com.github.windsekirun.rxsociallogin.intenal.net.OkHttpHelper
+import com.github.windsekirun.rxsociallogin.intenal.fuel.toResultObservable
 import com.github.windsekirun.rxsociallogin.model.LoginResultItem
 import com.github.windsekirun.rxsociallogin.model.PlatformType
 import com.nhn.android.naverlogin.OAuthLogin
 import com.nhn.android.naverlogin.OAuthLoginDefine
 import com.nhn.android.naverlogin.OAuthLoginHandler
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -64,40 +64,44 @@ class NaverLogin(activity: Activity) : SocialLogin(activity) {
     }
 
     private fun requestProfile(authHeader: String) {
-        val disposable = requestUrl.toRequest(authHeader)
+        val disposable = requestUrl.httpGet()
+                .header("Authorization" to authHeader)
+                .toResultObservable()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    val jsonObject = it.createJSONObject()
-                    val responseObject = getJSONObject(jsonObject, "response")
-
-                    if (responseObject == null) {
+                .subscribe { result, error ->
+                    if (error == null && result.component1() != null) {
+                        parseUserInfo(result.component1())
+                    } else {
                         responseFail(PlatformType.NAVER)
-                        return@subscribe
                     }
-
-                    val item = LoginResultItem().apply {
-                        this.id = responseObject.getJSONString("id")
-                        this.name = responseObject.getJSONString("name")
-                        this.email = responseObject.getJSONString("email")
-                        this.nickname = responseObject.getJSONString("nickname")
-                        this.gender = responseObject.getJSONString("gender")
-                        this.age = responseObject.getJSONString("age")
-                        this.birthday = responseObject.getJSONString("birthday")
-                        this.profilePicture = responseObject.getJSONString("profile_image")
-                        this.platform = PlatformType.NAVER
-                        this.result = true
-                    }
-
-                    responseSuccess(item)
-                }) {
-                    responseFail(PlatformType.NAVER)
                 }
 
         compositeDisposable.add(disposable)
     }
 
-    private fun String.toRequest(authorization: String): Single<String> {
-        return OkHttpHelper.get(this, "Authorization" to authorization)
+    private fun parseUserInfo(jsonStr: String?) {
+        val jsonObject = jsonStr?.createJSONObject()
+        val responseObject = getJSONObject(jsonObject, "response")
+
+        if (responseObject == null) {
+            responseFail(PlatformType.NAVER)
+            return
+        }
+
+        val item = LoginResultItem().apply {
+            this.id = responseObject.getJSONString("id")
+            this.name = responseObject.getJSONString("name")
+            this.email = responseObject.getJSONString("email")
+            this.nickname = responseObject.getJSONString("nickname")
+            this.gender = responseObject.getJSONString("gender")
+            this.age = responseObject.getJSONString("age")
+            this.birthday = responseObject.getJSONString("birthday")
+            this.profilePicture = responseObject.getJSONString("profile_image")
+            this.platform = PlatformType.NAVER
+            this.result = true
+        }
+
+        responseSuccess(item)
     }
 }
